02b7405d9db6e3939ec7fe646ae05660ebb73e8c
GulnarMammadzada <gulnarmmmdzad05@gmail.com>
2025-02-16 21:50:40 +0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,5 @@
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-//        int number = 12345;
-//        int reversed = 0;
-//        while (number > 0) {
-//            int digit = number % 10;
-//            reversed = reversed * 10 + digit;
-//            number /= 10;
-//        }
-//        System.out.println(reversed);
-//    }
-//
-//
-//    Scanner sc = new Scanner(System.in);
-//    int num;
-//
-//    do
-//
-//    {
-//        System.out.println("Enter num");
-//        num = sc.nextInt();
-//        if (num == 0) {
-//            System.out.println("Exiting");
-//        }
-//    }while(num>0);
-//
-//
-//
-//    for(
-//    int i = 1;
-//    i<=8;i++)
-//
-//    {
-//        for (int j = 1; j <= 8; j++) {
-//            System.out.println(i * j);
-//        }
-//
-//        for (int i = 1; i <= 8; i++) {
-//            for (int j = 1; j <= i; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-//
-//
-//        String[] names = {"ada", "aafa"};
-//        for (String name : names) {
-//            System.out.println(name);
-//        }
-
 
     }
 }
